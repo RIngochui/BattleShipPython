ca30a68f1f8dcc7052bb0a9046d6d8d6275670fc
Ringo Chui <Ringochewyis@gmail.com>
2020-10-29 21:53:12 +0000

    Finished the game for a single player experience

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from random import randint
+from os import system
 
 grid = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-#x = randint(10, 35)
-#y = randint(10, 35)
-# first commit
-
-x = 20
-y = 20
+x = randint(10, 35)
+y = randint(10, 35)
 
 ships = {
     "A": 10,
@@ -19,12 +16,11 @@ ships = {
 
 gameBoard = []
 hiddenBoard = []
-
 missleAway = 00
 missleLeft = 50
 score = 000
+move = ""
 end = 0
-
 
 def initGame():
     gameBoard[:] = [["~"] * x for i in range(y)]
@@ -41,11 +37,13 @@ def drawBoard():
         print(grid[count], "|", "".join(set), "|", grid[count])
         count += 1
     print("   ", grid[:x])
+    print("Missiles Away:", missleAway, "Missiles Left:", missleLeft)
+    print("Current Score:", score, "Last Move:", move)
 
 
 def loadShips(key):
-    tempRow = randint(0, y - 1)
     tempCol = randint(0, x - ships[key])
+    tempRow = randint(0, y - 1)
     # print(tempRow, tempCol)
     while (hiddenBoard[tempRow][tempCol:tempCol + ships[key]] !=
            ["~"] * ships[key]):
@@ -60,18 +58,80 @@ def loadShips(key):
         hiddenBoard[tempRow][tempCol + ships[key] - 2] = "="
 
 
-def updateData():
-    return (0)
+def updateData(coord):
+    global missleAway
+    global missleLeft
+    global score
+    global move
+    tempCoord = ""
+
+    count = 0
+    for index in str(coord):
+      count += 1
+
+    if (count % 2 != 0):
+      tempCoord = "0" + str(coord) 
+    else:
+      tempCoord = str(coord)
+
+    halfSize = int(len(tempCoord)/2)
+    tempCol = int(tempCoord[halfSize:])
+    tempRow = int(tempCoord[:halfSize])
+    
+    missleAway += 1
+    missleLeft -= 1
+    
+    if (hiddenBoard[tempRow][tempCol] == "~"):
+      gameBoard[tempRow][tempCol] = "X"
+      move = "MISS on " + grid[tempRow] + grid[tempCol]
+    else:
+      gameBoard[tempRow][tempCol] = hiddenBoard[tempRow][tempCol]
+      move = "HIT on " + grid[tempRow] + grid[tempCol]
+      score += 5
 
 
 def checkMove():
-    return (0)
+    coordinate = input("Enter Target Coordinates--> ")
+    tempCol = ""
+    tempRow = ""
+    for index in range(len(grid)):
+        if (coordinate[0] == grid[index]):
+            tempCol = index
+        if (coordinate[1] == grid[index]):
+            tempRow = index
+
+    if (tempRow < x and tempCol < y):
+        return (int(str(tempCol) + str(tempRow)))
+    else:
+        return (-1)
 
 
 def playBattleship():
-    initGame()
-    drawBoard()
+  global end
+  global missleLeft
+  global missleAway
+  initGame()
+  
+  playerNum = input("How many players? (1 or 0): ")
+  system('clear')
+
+  if(playerNum == "1"):
+    while(end == 0):
+      drawBoard()
+      updateData(checkMove())
+      if(score == 160):
+        end = 1
+        print("YOU WON, IN", missleAway, "MOVES")
+      elif (missleLeft == 0):
+        end = 1
+        print("YOU LOSE, RAN OUT OF MISSLES")
+      system('clear')
+  elif(playerNum == "0"):
+    return(0)
+  else:
+    print("NOT A VALID OPTION, PLEASE RUN PROGRAM AGAIN!!!")
+
 
 
 playBattleship()
-print(hiddenBoard)
+
